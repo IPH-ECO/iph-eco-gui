b005b10cabdba6a5a731a0df4afad8c0a535e796
João Roberto <joao@4techlabs.com.br>
2015-02-04 16:57:58 -0300

    Improving Unstructured Mesh Widget

--- include/ui/unstructured_mesh_widget.h
+++ include/ui/unstructured_mesh_widget.h
@@ -2,6 +2,8 @@
 #define UNSTRUCTURED_MESH_WIDGET_H
 
 #include <QWidget>
+#include <QFileDialog>
+#include <QInputDialog>
 
 namespace Ui {
 class UnstructuredMeshWidget;
@@ -14,6 +16,11 @@ class UnstructuredMeshWidget : public QWidget
 public:
     explicit UnstructuredMeshWidget(QWidget *parent = 0);
     ~UnstructuredMeshWidget();
+
+private slots:
+    void on_newMeshButton_clicked();
+    void on_boundaryFileBrowserButton_clicked();
+    void on_resetMeshButton_clicked();
 
 private:
     Ui::UnstructuredMeshWidget *ui;
--- src/ui/unstructured_mesh_widget.cpp
+++ src/ui/unstructured_mesh_widget.cpp
@@ -1,6 +1,8 @@
 #include "include/ui/unstructured_mesh_widget.h"
 #include "ui_unstructured_mesh_widget.h"
 
+#include <QDebug>
+
 UnstructuredMeshWidget::UnstructuredMeshWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::UnstructuredMeshWidget)
@@ -8,7 +10,20 @@ UnstructuredMeshWidget::UnstructuredMeshWidget(QWidget *parent) :
     ui->setupUi(this);
 }
 
-UnstructuredMeshWidget::~UnstructuredMeshWidget()
-{
+void UnstructuredMeshWidget::on_newMeshButton_clicked() {
+    qDebug() << QInputDialog::getText(this, tr("Enter the mesh name"), tr("Mesh name"));
+}
+
+void UnstructuredMeshWidget::on_boundaryFileBrowserButton_clicked() {
+    QString filepath = QFileDialog::getOpenFileName(this, tr("Select a boundary file"), ".", "Keyhole Markup Language files (*.kml)");
+    ui->boundaryFileLineEdit->setText(filepath);
+}
+
+void UnstructuredMeshWidget::on_resetMeshButton_clicked() {
+    ui->minimumAngleSpinBox->setValue(ui->minimumAngleSpinBox->minimum());
+    ui->maximumEdgeLengthSpinBox->setValue(ui->maximumEdgeLengthSpinBox->minimum());
+}
+
+UnstructuredMeshWidget::~UnstructuredMeshWidget() {
     delete ui;
 }
