0da8b451c56e46636ccf85f8b2dbd515df6a8f6b
João Roberto <joao@4techlabs.com.br>
2015-02-10 22:36:51 -0300

    Changes on unstructured mesh generation form

--- src/ui/unstructured_mesh_widget.cpp
+++ src/ui/unstructured_mesh_widget.cpp
@@ -14,23 +14,6 @@ UnstructuredMeshWidget::~UnstructuredMeshWidget() {
     delete ui;
 }
 
-void UnstructuredMeshWidget::on_btnNewMesh_clicked() {
-    QString meshName = QInputDialog::getText(this, tr("Enter the mesh name"), tr("Mesh name"));
-
-    if (!meshName.isEmpty()) {
-        try {
-            meshService->addMesh(meshName);
-
-            ui->cbxMeshName->addItem(meshName);
-            ui->cbxMeshName->setCurrentText(meshName);
-
-            enableMeshForm(true);
-        } catch(MeshException &ex) {
-            QMessageBox::warning(this, tr("New unstructured mesh"), ex.what());
-        }
-    }
-}
-
 void UnstructuredMeshWidget::on_btnBoundaryFileBrowser_clicked() {
     QString boundaryFilePath = QFileDialog::getOpenFileName(this, tr("Select a boundary file"), ".", "Keyhole Markup Language file (*.kml)");
 
@@ -42,7 +25,9 @@ void UnstructuredMeshWidget::on_btnBoundaryFileBrowser_clicked() {
     }
 }
 
-void UnstructuredMeshWidget::on_btnGenerateMesh_clicked() {    
+void UnstructuredMeshWidget::on_btnGenerateMesh_clicked() {
+    enableMeshForm(true);
+
     QFile file(ui->edtBoundaryFileLine->text());
     QFileInfo fileInfo(file);
 
@@ -58,7 +43,6 @@ void UnstructuredMeshWidget::on_btnGenerateMesh_clicked() {
 
         if (!progressDialog->wasCanceled()) {
             //TODO: Draw boundary on OpenGL widget
-            enableMeshOptions(true);
         }
     } else {
         QMessageBox::warning(this, tr("Generate Mesh"), "Boundary file not found.");
@@ -68,32 +52,12 @@ void UnstructuredMeshWidget::on_btnGenerateMesh_clicked() {
 void UnstructuredMeshWidget::on_btnResetMesh_clicked() {
     ui->sbxMinimumAngle->setValue(ui->sbxMinimumAngle->minimum());
     ui->sbxMaximumEdgeLength->setValue(ui->sbxMaximumEdgeLength->minimum());
-    enableMeshOptions(false);
 }
 
-void UnstructuredMeshWidget::enableMeshForm(bool enable) {
-    ui->btnEditMesh->setEnabled(enable);
-    ui->btnRemoveMesh->setEnabled(enable);
-
-    ui->cbxMeshName->setEnabled(enable);
-    ui->edtBoundaryFileLine->setEnabled(enable);
-    ui->btnBoundaryFileBrowser->setEnabled(enable);
-
-    ui->sbxMaximumEdgeLength->setEnabled(enable);
-    ui->sbxMinimumAngle->setEnabled(enable);
-
-    ui->btnGenerateMesh->setEnabled(enable);
-    ui->btnResetMesh->setEnabled(enable);
-}
-
-void UnstructuredMeshWidget::enableMeshOptions(bool enable) {
-    ui->btnRefineSpecificRegion->setEnabled(enable);
-    ui->btnApplyCoarsening->setEnabled(enable);
-
-    ui->chkShowEdges->setEnabled(enable);
-    ui->chkShowTriangles->setEnabled(enable);
-    ui->chkShowUtmCoordinates->setEnabled(enable);
-    ui->chkShowVertexes->setEnabled(enable);
+void UnstructuredMeshWidget::on_btnSaveMesh_clicked() {
+    ui->cbxMeshName->addItem(ui->edtMeshName->text());
+    ui->cbxMeshName->setCurrentIndex(-1);
+    resetMeshForm();
 }
 
 void UnstructuredMeshWidget::on_btnRemoveMesh_clicked() {
@@ -104,31 +68,50 @@ void UnstructuredMeshWidget::on_btnRemoveMesh_clicked() {
         meshService->removeMesh(meshName);
 
         ui->cbxMeshName->removeItem(ui->cbxMeshName->currentIndex());
-
-        if (ui->cbxMeshName->count() == 0) {
-            enableMeshForm(false);
-            enableMeshOptions(false);
-        }
+        ui->cbxMeshName->setCurrentIndex(-1);
     }
-
 }
 
-void UnstructuredMeshWidget::on_btnEditMesh_clicked() {
-    QString oldMeshName = ui->cbxMeshName->currentText();
-    QString newMeshName = QInputDialog::getText(this, tr("Enter the mesh name"), tr("Mesh name"), QLineEdit::Normal, oldMeshName);
-
-    ui->cbxMeshName->removeItem(ui->cbxMeshName->currentIndex());
-
-    meshService->removeMesh(oldMeshName);
-    meshService->addMesh(newMeshName);
-
-    ui->cbxMeshName->addItem(newMeshName);
-    ui->cbxMeshName->setCurrentText(newMeshName);
+void UnstructuredMeshWidget::on_btnCancelMesh_clicked() {
+    resetMeshForm();
+    ui->cbxMeshName->setCurrentIndex(-1);
 }
 
-void UnstructuredMeshWidget::on_cbxMeshName_currentIndexChanged(int index)
-{
-    if (index < 0) {
-        enableMeshOptions(false); //TODO precisa verificar se há uma malha gerada
+void UnstructuredMeshWidget::on_cbxMeshName_currentIndexChanged(int index) {
+    if (index > -1) {
+        enableMeshForm(true);
+        ui->btnRemoveMesh->setEnabled(true);
+
+        //TODO: Load strings from mesh
+        ui->edtMeshName->setText(ui->cbxMeshName->currentText());
+        ui->edtBoundaryFileLine->setText("TODO");
+        ui->sbxMinimumAngle->setValue(0.125);
+        ui->sbxMaximumEdgeLength->setValue(0.05);
+    } else {
+        resetMeshForm();
     }
+}
+
+void UnstructuredMeshWidget::enableMeshForm(bool enable) {
+    ui->btnRefineSpecificRegion->setEnabled(enable);
+    ui->btnApplyCoarsening->setEnabled(enable);
+
+    ui->chkShowEdges->setEnabled(enable);
+    ui->chkShowTriangles->setEnabled(enable);
+    ui->chkShowUtmCoordinates->setEnabled(enable);
+    ui->chkShowVertexes->setEnabled(enable);
+
+    ui->btnSaveMesh->setEnabled(enable);
+    ui->btnCancelMesh->setEnabled(enable);
+}
+
+void UnstructuredMeshWidget::resetMeshForm() {
+    ui->edtMeshName->setFocus();
+
+    ui->edtMeshName->clear();
+    ui->edtBoundaryFileLine->clear();
+    on_btnResetMesh_clicked();
+
+    ui->btnRemoveMesh->setEnabled(false);
+    enableMeshForm(false);
 }
