80efa092713e36f229715f18e0dfe6294e4cc120
João Roberto <joao@4techlabs.com.br>
2015-12-16 08:29:59 -0300

    AbstractDialogAction enumeration

--- include/ui/abstract_mesh_dialog.h
+++ include/ui/abstract_mesh_dialog.h
@@ -9,6 +9,19 @@
 #include <QSettings>
 #include <QShowEvent>
 
+enum class AbstractDialogAction {
+    RESET_ZOOM = (1 << 0),
+    AREA_ZOOM = (1 << 1),
+    MESH_VISIBILITY = (1 << 2),
+    AXES_VISIBILITY = (1 << 3),
+    CELL_IDS_VISIBILITY = (1 << 4),
+    VERTICE_IDS_VISIBILITY = (1 << 5),
+    MESH_PROPERTIES = (1 << 6),
+    EXPORT_AS_PICTURE = (1 << 7),
+    EXPORT_AS_VIDEO = (1 << 8),
+    CHANGE_BACKGROUND_COLOR = (1 << 9)
+};
+
 class AbstractMeshDialog : public QDialog {
 	Q_OBJECT
 protected:
