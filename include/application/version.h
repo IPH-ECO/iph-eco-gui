8d742906d31b80e3b71e62503d0f5b02d28fa837
João Roberto <joao@4techlabs.com.br>
2016-01-24 10:27:45 -0300

    Including version information

---
+++ include/application/version.h
@@ -0,0 +1,21 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VER_FILEVERSION             0,1
+#define VER_FILEVERSION_STR         "0.1\0"
+
+#define VER_PRODUCTVERSION          0,1
+#define VER_PRODUCTVERSION_STR      "0.1\0"
+
+#define VER_COMPANYNAME_STR         "IPH/UFRGS"
+#define VER_FILEDESCRIPTION_STR     "IPH-ECO"
+#define VER_INTERNALNAME_STR        "IPH-ECO"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2016 IPH/UFRGS"
+#define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
+#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR    "iph-eco.exe"
+#define VER_PRODUCTNAME_STR         "IPH-ECO"
+
+#define VER_COMPANYDOMAIN_STR       "https://sites.google.com/site/iphecomodel"
+
+#endif // VERSION_H
