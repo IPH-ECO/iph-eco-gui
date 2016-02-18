7f1b6388c2558974acf448028ebea8a482f3071f
Joao Roberto <joaorobertojr88@gmail.com>
2016-02-18 08:09:48 -0300

    Reading argv parameter in main application function

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <application/iph_application.h>
 #include <ui/main_window.h>
 
-#include <clocale>
-
+#ifdef _WIN32
+int wmain(int argc, wchar_t *argv[], wchar_t *envp[]) {
+	IPHApplication a(argc, (char **)argv);
+#else
 int main(int argc, char *argv[]) {
-	std::setlocale(LC_ALL, "C");
-
-    IPHApplication a(argc, argv);
+	IPHApplication a(argc, argv);
+#endif
 
     QApplication::setOrganizationDomain("4techlabs.com.br");
     QApplication::setOrganizationName("4techlabs");
@@ -16,6 +17,15 @@ int main(int argc, char *argv[]) {
     MainWindow *mainWindow = new MainWindow();
     mainWindow->showMaximized();
     IPHApplication::setMainWindow(mainWindow);
+
+	if (argc > 1) {
+#ifdef _WIN32
+		QString filename = QString::fromWCharArray(argv[1]);
+#else
+		QString filename = argv[1];
+#endif
+		mainWindow->openProject(filename);
+	}
 
     return a.exec();
 }
