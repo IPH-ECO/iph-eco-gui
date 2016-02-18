00e19885886de5c29f2a3a1f02757e6722f46c67
João Roberto <joao@4techlabs.com.br>
2016-02-18 18:43:04 -0300

    Fixing bug when stopping a running simulation

--- src/domain/simulation_data_type.cpp
+++ src/domain/simulation_data_type.cpp
@@ -1,5 +1,15 @@
 #include <domain/simulation_data_type.h>
 
+SimulationDataType::StructuredMesh::StructuredMesh() :
+    xCoordinates(nullptr),
+    yCoordinates(nullptr),
+    northNeighbors(nullptr),
+    westNeighbors(nullptr),
+    southNeighbors(nullptr),
+    eastNeighbors(nullptr),
+    verticeIds(nullptr)
+{}
+
 void SimulationDataType::StructuredMesh::destroy() {
 	delete xCoordinates;
     delete yCoordinates;
@@ -10,15 +20,31 @@ void SimulationDataType::StructuredMesh::destroy() {
     delete verticeIds;
 }
 
+SimulationDataType::UnstructuredMesh::UnstructuredMesh() :
+    xCoordinates(nullptr),
+    yCoordinates(nullptr),
+    verticeIds(nullptr)
+{}
+
 void SimulationDataType::UnstructuredMesh::destroy() {
 	delete xCoordinates;
     delete yCoordinates;
     delete verticeIds;
 }
 
+SimulationDataType::GridData::GridData() :
+    weights(nullptr)
+{}
+
 void SimulationDataType::GridData::destroy() {
     delete weights;
 }
+
+SimulationDataType::GridDataConfiguration::GridDataConfiguration() :
+    layers(nullptr),
+    structuredMesh(nullptr),
+    unstructuredMesh(nullptr)
+{}
 
 void SimulationDataType::GridDataConfiguration::destroy() {
 	layers->destroy();
@@ -35,29 +61,57 @@ void SimulationDataType::GridDataConfiguration::destroy() {
     }
 }
 
+SimulationDataType::HydrodynamicParameter::HydrodynamicParameter() :
+    name(nullptr)
+{}
+
 void SimulationDataType::HydrodynamicParameter::destroy() {
     delete name;
 }
+
+SimulationDataType::BoundaryCondition::BoundaryCondition() :
+    cells(nullptr),
+    timeSeriesList(nullptr)
+{}
 
 void SimulationDataType::BoundaryCondition::destroy() {
 	delete cells;
     delete timeSeriesList;
 }
 
+SimulationDataType::MeteorologicalParameter::MeteorologicalParameter() :
+    name(nullptr),
+    timeSeriesList(nullptr)
+{}
+
 void SimulationDataType::MeteorologicalParameter::destroy() {
 	delete name;
     delete timeSeriesList;
 }
+
+SimulationDataType::MeteorologicalStation::MeteorologicalStation() :
+    parameters(nullptr)
+{}
 
 void SimulationDataType::MeteorologicalStation::destroy() {
 	parameters->destroy();
     delete parameters;
 }
 
+SimulationDataType::MeteorologicalConfiguration::MeteorologicalConfiguration() :
+    stations(nullptr)
+{}
+
 void SimulationDataType::MeteorologicalConfiguration::destroy() {
 	stations->destroy();
     delete stations;
 }
+
+SimulationDataType::HydrodynamicConfiguration::HydrodynamicConfiguration() :
+    parameters(nullptr),
+    boundaryConditions(nullptr),
+    gridDataConfiguration(nullptr)
+{}
 
 void SimulationDataType::HydrodynamicConfiguration::destroy() {
 	parameters->destroy();
@@ -69,15 +123,36 @@ void SimulationDataType::HydrodynamicConfiguration::destroy() {
     delete gridDataConfiguration;
 }
 
+SimulationDataType::OutputParameter::OutputParameter() :
+    name(nullptr)
+{}
+
 void SimulationDataType::OutputParameter::destroy() {
     delete name;
 }
+
+SimulationDataType::RecoveryVariables::RecoveryVariables() :
+    u(nullptr),
+    w(nullptr),
+    eta(nullptr)
+{}
 
 void SimulationDataType::RecoveryVariables::destroy() {
 	delete u;
     delete w;
     delete eta;
 }
+
+SimulationDataType::Simulation::Simulation() :
+    label(nullptr),
+    layers(nullptr),
+    hydrodynamic(nullptr),
+    meteorologicalConfiguration(nullptr),
+    observations(nullptr),
+    outputDirectory(nullptr),
+    outputParameters(nullptr),
+    recoveryVariables(nullptr)
+{}
 
 void SimulationDataType::Simulation::destroy() {
 	hydrodynamicConfiguration->destroy();
